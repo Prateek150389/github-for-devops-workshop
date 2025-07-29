d06af3d7160dbc55018d869d350a312efaa80f4d
Prateek Kumar <prateek.drishti@gmail.com>
2025-07-29 10:23:48 +0530

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,4 +1,13 @@
-print("Hello Doston")
+def my_function():
+    a = 5
+    b = 6
+    c = 8
+    password = "prateek@123"
+    d = a + b + c
+    print(d)
 
-print("I am learning git & github")
+    return str(d) + password
 
+
+result = my_function()
+print(result)
